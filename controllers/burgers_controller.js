e718a470a29aea8872886eb0230f351a87e92baf
Maya Stucky <mayaatstucky@gmail.com>
2020-07-14 19:45:01 -0400

    styling and getting devoured button to work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -43,18 +43,13 @@ router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
   console.log("put route");
 
-  burger.updateOne(
-    "burgers",
-    req.body,
-    parseInt(req.params.id),
-    function (result) {
-      if (result.changedRows == 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+  burger.updateOne(true, parseInt(req.params.id), function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
-  );
+  });
 });
 
 module.exports = router;
